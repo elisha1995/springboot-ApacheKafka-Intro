60261d20c8c3b60d256c4bbcbb5fac260f9b60c0
elisha1995 <elishasamuel01@gmail.com>
2024-10-20 22:31:17 +0000

    Create REST API to Send JSON Object

--- src/main/java/ges/javaguides/springbootkafka/kafka/JsonKafkaProducer.java
+++ src/main/java/ges/javaguides/springbootkafka/kafka/JsonKafkaProducer.java
@@ -26,10 +26,9 @@ public class JsonKafkaProducer {
 
         Message<User> message = MessageBuilder
                 .withPayload(data)
-                .setHeader(KafkaHeaders.TOPIC, "javaguides")
+                .setHeader(KafkaHeaders.TOPIC, "javaguides_json")
                 .build();
 
         kafkaTemplate.send(message);
     }
-
 }
